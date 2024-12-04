999221eea71f2066c734f7cb79c6889734836ffe
venkatesh08-DreamTusk <venkatesh.r@dreamtusk.com>
2024-12-04 10:35:23 +0530

    updated

---
+++ Day_1/PrimeNumber/PrimeNumber.java
@@ -0,0 +1,25 @@
+package Day_1.PrimeNumber;
+
+public class PrimeNumber {
+    public static  void main(String[] args){
+        int num = 1;
+        Boolean result = isPrime(num);
+
+        if (result) {
+            System.out.println("Prime Number");
+        } else {
+            System.out.println("Not Prime Number");
+        }
+    }
+    private static  Boolean isPrime(int number){
+        int count = 0;
+
+        for(int i = 1; i <= number ;i++){
+            if(number/i == 0){
+                count++;
+            }
+        }
+
+        return count == 2;
+    }
+}
